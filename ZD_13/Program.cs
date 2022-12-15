451d5f4db441d8324464dd2d569dc0998a7aafb8
Viktoria <vikatim4encko@yandex.ru>
2022-12-15 17:48:32 +0300

    Задача №13

---
+++ ZD_13/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = int.Parse(Console.ReadLine());
+if(a.ToString().Length<3){
+    Console.WriteLine($"Третьей цифры в числе {a} нет."); 
+}
+else
+{
+    int b = a;
+    int ost;
+    for(int i=0;i<a.ToString().Length;i++)
+    {
+            ost = b%10; 
+            b = b/10;
+        if(a.ToString().Length - i == 3)
+        {
+            Console.WriteLine($"Третья цифра в числе {a} - {ost}");
+        }
+    }
+    
+}
+ 
