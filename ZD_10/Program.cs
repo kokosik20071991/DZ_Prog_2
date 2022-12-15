9dd6d7e2fdf0bbe11f442fc36d90755ae482f032
Viktoria <vikatim4encko@yandex.ru>
2022-12-15 13:00:30 +0300

    Задача №10

---
+++ ZD_10/Program.cs
@@ -0,0 +1,8 @@
+﻿int a;
+Console.Write("Введите трехзначное число: ");
+int.TryParse(Console.ReadLine()!, out a);
+System.Console.WriteLine(); 
+
+System.Console.WriteLine($"Второй цифрой числа {a} является {a / 10 % 10} ");
+
+System.Console.WriteLine(); 
