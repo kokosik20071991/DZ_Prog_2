86f369145b4c2d065b136ba78961dba549255883
Viktoria <vikatim4encko@yandex.ru>
2022-12-15 13:03:56 +0300

    Задача №15

---
+++ ZD_15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine();
+int Day = new Random().Next(1, 8);
+Console.WriteLine();
+Console.WriteLine($"Дано число: {Day}");
+if (Day==6|| Day==7) Console.WriteLine("Это Выходной!!!");
+else
+ Console.WriteLine("Рабочий!");
